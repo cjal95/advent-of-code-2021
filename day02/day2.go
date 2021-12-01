83620eef87f9df0408ed33815de9440cbca6a015
cjal95 <60397509+cjal95@users.noreply.github.com>
2021-12-01 09:43:31 +0100

    Cleanup

---
+++ day02/day2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	//"bufio"
+	"fmt"
+	//"os"
+	//"strconv"
+)
+
+func part1() {
+	fmt.Println("Part 1")
+}
+
+func part2() {
+	fmt.Println("Part 2")
+}
+
+/*func getMeasurements() []int {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var numbers []int
+	for scanner.Scan() {
+		x, _ := strconv.Atoi(scanner.Text())
+		numbers = append(numbers, x)
+	}
+	return numbers
+}*/
+
+func main() {
+	part1()
+	part2()
+	//fmt.Println("Part one", part1(numbers))
+	//fmt.Println("Part two", part2(numbers))
+}
